e12d88b5b3ed7e724594b1bf668f6c4b2372a18b
Bogdan Salyp <bogdansalyp@gmail.com>
2025-06-20 23:49:17 +0000

    fix: Trailing whitespaces as dots

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,6 +2,11 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
+-- showing whitespaces
+vim.cmd("set list")
+vim.cmd("set listchars+=trail:·")
+
 vim.g.mapleader = " "
 
 vim.keymap.set('i', 'df', '<Esc>', { noremap = true })
