8e84fe6f07bed5af52d558488eddeb2cc11f25f2
Bogdan Salyp <bogdansalyp@gmail.com>
2025-10-03 17:01:34 -0700

    fix: Removed .git from telescope and added recent files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,16 +10,21 @@ return {
       require("telescope").setup({
         defaults = {
           hidden = true,
+          no_ignore = false,
+          file_ignore_patterns = { ".git/" },
         },
         pickers = {
           find_files = {
             hidden = true,
+            no_ignore = false,
+            file_ignore_patterns = { ".git/" },
           },
         },
       })
       local builtin = require("telescope.builtin")
       vim.keymap.set('n', '<space>o', builtin.find_files)
       vim.keymap.set('n', '<space>g', builtin.live_grep)
+      vim.keymap.set("n", "<space>fr", builtin.oldfiles, { noremap = true, silent = true })
     end
   }
 }
