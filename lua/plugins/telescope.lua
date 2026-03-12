0a1e83a70aa22b8ceab8c1e09a8e9139fc76cd8d
Bogdan Salyp <bogdansalyp@gmail.com>
2026-03-12 03:44:32 +0000

    fix: Compare files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -25,6 +25,18 @@ return {
       vim.keymap.set('n', '<space>o', builtin.find_files)
       vim.keymap.set('n', '<space>g', builtin.live_grep)
       vim.keymap.set("n", "<space>fr", builtin.oldfiles, { noremap = true, silent = true })
+      vim.keymap.set('n', '<space>d', function()
+        builtin.find_files({
+          attach_mappings = function(_, map)
+            map("i", "<CR>", function(prompt_bufnr)
+              local entry = require("telescope.actions.state").get_selected_entry(prompt_bufnr)
+              require("telescope.actions").close(prompt_bufnr)
+              vim.cmd("vert diffsplit " .. entry.path)
+            end)
+            return true
+          end,
+        })
+      end)
     end
   }
 }
